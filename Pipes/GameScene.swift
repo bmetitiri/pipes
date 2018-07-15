6b27c0e2a6244e323ebc4a93fda41b88aa6d44f8
bmetitiri <beametitiri@gmail.com>
2018-07-15 08:10:48 -0700

    Added background grid

--- Pipes/GameScene.swift
+++ Pipes/GameScene.swift
@@ -12,16 +12,14 @@ class GameScene: SKScene {
   // MARK:  Game stuff
   var map: Map<GameNode>! {
     didSet {
-      addSprites(for: map.map.flatMap { return $0 })
-      updateCameraConstraints()
+      updateMap()
     }
   }
 
-  let tileWidth: CGFloat = 32.0
-  let tileHeight: CGFloat = 32.0
+  let tileSize: CGFloat = 32.0
 
   // MARK: Layers
-  let gameLayer = SKNode()
+  let gameLayer = SKSpriteNode()
   let nodeLayer = SKNode()
 
   // MARK: Gesture stuff
@@ -31,6 +29,7 @@ class GameScene: SKScene {
 
   override func didMove(to view: SKView) {
     addChild(gameLayer)
+    gameLayer.anchorPoint = anchorPoint
     //
     //    let layerPosition = CGPoint(
     //      x: -TileWidth * CGFloat(NumColumns) / 2,
@@ -42,7 +41,6 @@ class GameScene: SKScene {
     let camera = SKCameraNode()
     addChild(camera)
     self.camera = camera
-
     setupGestures()
   }
 
@@ -58,17 +56,28 @@ class GameScene: SKScene {
   func addSprites(for nodes: [GameNode]) {
     for node in nodes where node.value != nil {
       let sprite = SKSpriteNode(imageNamed: node.value!.type.spriteName)
-      sprite.size = CGSize(width: tileWidth, height: tileHeight)
-      sprite.position = pointFor(column: node.column, row: node.row)
+      sprite.size = CGSize(width: tileSize, height: tileSize)
+      sprite.position = spritePositionFor(column: node.column, row: node.row)
       nodeLayer.addChild(sprite)
       node.sprite = sprite
     }
   }
+
+  // MARK: - Helper functions
   
-  private func pointFor(column: Int, row: Int) -> CGPoint {
+  private func spritePositionFor(column: Int, row: Int) -> CGPoint {
     return CGPoint(
-      x: CGFloat(column) * tileWidth + tileWidth / 2,
-      y: CGFloat(row) * tileHeight + tileHeight / 2)
+      x: CGFloat(column) * tileSize + tileSize / 2,
+      y: CGFloat(row) * tileSize + tileSize / 2)
+  }
+
+  func updateMap() {
+    addSprites(for: map.map.flatMap { return $0 })
+    updateCameraConstraints()
+    guard let texture = gridTexture(rows: map.height, cols: map.width, tileSize: tileSize)
+      else { return }
+    gameLayer.size = CGSize(width: texture.size().width, height: texture.size().height)
+    gameLayer.texture = texture
   }
 
   private func updateCameraConstraints() {
@@ -82,6 +91,38 @@ class GameScene: SKScene {
     let yRange = SKRange(lowerLimit: insetRect.minY, upperLimit: insetRect.maxY)
     let cameraEdgeConstraint = SKConstraint.positionX(xRange, y: yRange)
     camera.constraints = [cameraEdgeConstraint]
+  }
+
+  private func gridTexture(rows: Int, cols: Int, tileSize: CGFloat) -> SKTexture? {
+    // Add 1 to the height and width to ensure the borders are within the sprite
+    let size = CGSize(width: CGFloat(cols) * tileSize + 1, height: CGFloat(rows) * tileSize + 1)
+    UIGraphicsBeginImageContext(size)
+
+    guard let context = UIGraphicsGetCurrentContext() else {
+      return nil
+    }
+    let bezierPath = UIBezierPath()
+    let offset: CGFloat = 0.5
+    // Draw vertical lines
+    for i in 0...cols {
+      let x = CGFloat(i) * tileSize + offset
+      bezierPath.move(to: CGPoint(x: x, y: 0))
+      bezierPath.addLine(to: CGPoint(x: x, y: size.height))
+    }
+    // Draw horizontal lines
+    for i in 0...rows {
+      let y = CGFloat(i) * tileSize + offset
+      bezierPath.move(to: CGPoint(x: 0, y: y))
+      bezierPath.addLine(to: CGPoint(x: size.width, y: y))
+    }
+    SKColor.white.setStroke()
+    bezierPath.lineWidth = 1.0
+    bezierPath.stroke()
+    context.addPath(bezierPath.cgPath)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    return SKTexture(image: image!)
   }
 
 }

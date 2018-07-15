7ac945bf3a27265d1b29a278ea34a615990b181b
bmetitiri <beametitiri@gmail.com>
2018-07-15 03:13:40 -0700

    Added camera and basic panning

--- Pipes/GameScene.swift
+++ Pipes/GameScene.swift
@@ -9,29 +9,44 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-  var level: Map<GameNode>!
+  // MARK:  Game stuff
+  var map: Map<GameNode>! {
+    didSet {
+      updateSize()
+    }
+  }
 
   let tileWidth: CGFloat = 32.0
   let tileHeight: CGFloat = 32.0
 
+  // MARK: Layers
   let gameLayer = SKNode()
   let nodeLayer = SKNode()
 
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
+  // MARK: Gesture variables
+  private var lastPosition: CGPoint = CGPoint.zero
+
+  override func didMove(to view: SKView) {
+    addChild(gameLayer)
+    //
+    //    let layerPosition = CGPoint(
+    //      x: -TileWidth * CGFloat(NumColumns) / 2,
+    //      y: -TileHeight * CGFloat(NumRows) / 2)
+    //
+    //    cookiesLayer.position = layerPosition
+    gameLayer.addChild(nodeLayer)
+
+    let camera = SKCameraNode()
+    addChild(camera)
+    self.camera = camera
+
+    setupGestures()
   }
 
-  override init(size: CGSize) {
-    super.init(size: size)
-
-    addChild(gameLayer)
-//
-//    let layerPosition = CGPoint(
-//      x: -TileWidth * CGFloat(NumColumns) / 2,
-//      y: -TileHeight * CGFloat(NumRows) / 2)
-//
-//    cookiesLayer.position = layerPosition
-    gameLayer.addChild(nodeLayer)
+  private func setupGestures() {
+    let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panRecognized))
+    panGesture.maximumNumberOfTouches = 1
+    view?.addGestureRecognizer(panGesture)
   }
 
   func addSprites(for nodes: [GameNode]) {
@@ -48,6 +63,24 @@ class GameScene: SKScene {
     return CGPoint(
       x: CGFloat(column) * tileWidth + tileWidth / 2,
       y: CGFloat(row) * tileHeight + tileHeight / 2)
+  }
+
+  private func updateSize() {
+    size = CGSize(width: tileWidth * CGFloat(map.width),
+                  height: tileHeight * CGFloat(map.height))
+  }
+
+}
+
+// MARK: - Gesture Recognizers
+
+extension GameScene {
+
+  @objc func panRecognized(_ sender: UIPanGestureRecognizer) {
+    let translation = sender.translation(in: view!)
+    sender.setTranslation(CGPoint.zero, in: view)
+    guard let position = camera?.position else { return }
+    camera?.position = CGPoint(x: position.x - translation.x, y: position.y + translation.y)
   }
 
 }
